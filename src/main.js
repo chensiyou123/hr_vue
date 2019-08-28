44eef87e6a2b5e30b109e92e547cb8b897a033ab
wujun <13550081270@qq.com>
2019-08-28 21:04:46 +0800

    feat:"路由前拦截"

--- src/main.js
+++ src/main.js
@@ -4,12 +4,29 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import store from './store'
 import 'element-ui/lib/theme-chalk/index.css'
 import {postRequest} from './utils/api'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 Vue.prototype.postRequest = postRequest;
+
+router.beforeEach((to, from, next)=>{
+  debugger
+  if (to.name == 'Login') {
+    next();
+    return;
+  }
+  var name = store.state.user.name;
+  if (name == '未登录') {
+    if (to.meta.requireAuth || to.name == null) {
+      next({path: '/', query: {redirect: to.path}})
+    }else {
+      next();
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
 Vue.use(Router)
 
 export default new Router({
@@ -8,7 +9,17 @@ export default new Router({
     {
       path: '/',
       name: 'Login',
-      component: Login
+      component: Login,
+      hidden: true,
+    },
+    {
+      path: '/home',
+      name: '主页',
+      component: Home,
+      hidden: true,
+      meta: {
+        requireAuth: true
+      },
     }
   ]
 })
