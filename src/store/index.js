57c22a38cbea0581814a1f8996568fc589f7101f
wujun <13550081270@qq.com>
2019-08-31 11:27:59 +0800

    feat:"socket测试"

--- src/store/index.js
+++ src/store/index.js
@@ -32,6 +32,36 @@ export default new Vuex.Store({
   actions:{
     connect(context){
       context.state.stomp = Stomp.over(new SockJS("/ws/endpointChat"));
+      context.state.stomp.connect({}, frame=> {
+        context.state.stomp.subscribe("/user/queue/chat",message=>{
+          var msg = JSON.parse(message.body);
+          var oldMsg = window.localStorage.getItem(context.state.user.username + "#" + msg.from);
+          if (oldMsg == null) {
+            oldMsg = [];
+            oldMsg.push(msg);
+            window.localStorage.setItem(context.state.user.username + "#" + msg.from, JSON.stringify(oldMsg))
+          } else {
+            var oldMsgJson = JSON.parse(oldMsg);
+            oldMsgJson.push(msg);
+            window.localStorage.setItem(context.state.user.username + "#" + msg.from, JSON.stringify(oldMsgJson))
+          }
+          if (msg.from != context.state.currentFriend.username) {
+            context.commit("addValue2DotMap", "isDot#" + context.state.user.username + "#" + msg.from);
+          }
+          //更新msgList
+          var oldMsg2 = window.localStorage.getItem(context.state.user.username + "#" + context.state.currentFriend.username);
+          if (oldMsg2 == null) {
+            context.commit('updateMsgList', []);
+          } else {
+            context.commit('updateMsgList', JSON.parse(oldMsg2));
+          }
+        });
+        context.state.stomp.subscribe("/topic/nf", message=> {
+          context.commit('toggleNFDot', true);
+        });
+      },failedMsg=> {
+
+      })
     }
   }
 })
