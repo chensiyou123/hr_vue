d623d2853f7e16a3f90fd2a14fcba44b6021eaa1
wujun <13550081270@qq.com>
2019-08-31 18:53:28 +0800

    feat:"集成分页（里程碑版本）"

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {Message} from 'element-ui'
 let base = '';
 export const postRequest=(url, params)=>{
   return axios({
@@ -24,4 +25,34 @@ export const getRequest=((url)=>{
     method: 'get',
     url: `${base}${url}`
   });
+})
+
+axios.interceptors.request.use(config => {
+  return config;
+}, err => {
+  Message.error({message: '请求超时!'});
+})
+axios.interceptors.response.use(data=>{
+  if (data.status && data.status == 200 && data.data.status == 500) {
+    Message.error({message: data.data.msg});
+    return;
+  }
+  if (data.data.msg) {
+    Message.success({message: data.data.msg});
+  }
+  return data;
+},err=>{
+  if (err.response.status == 504 || err.response.status == 404) {
+    Message.error({message: '服务器被吃了⊙﹏⊙∥'});
+  } else if (err.response.status == 403) {
+    Message.error({message: '权限不足,请联系管理员!'});
+  }else if (err.response.status == 401) {
+    Message.error({message: err.response.data.msg});
+  } else {
+    if (err.response.data.msg) {
+      Message.error({message: err.response.data.msg});
+    }else{
+      Message.error({message: '未知错误!'});
+    }
+  }
 })
